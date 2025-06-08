b1329c91639fb7e37b1f1820cfd395def277a031
KARTHIK <144221056+KaRthiK15789@users.noreply.github.com>
2025-06-09 01:17:30 +0530

    Update vite.config.ts

--- Image_Processor/vite.config.ts
+++ Image_Processor/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': process.env
   },
-  plugins: [
-  react()],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-}));
+  build: {
+    rollupOptions: {
+      external: [
+        'fs',
+        'path',
+        'pdf-parse',
+        'mammoth',
+        'image-to-text'
+      ],
+    },
+  },
+});
